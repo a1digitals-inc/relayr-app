43f3d8745427d785725e5e9ac41c1c6b53d44299
andre <andrleite@gmail.com>
2019-06-29 00:39:23 -0300

    disable drop database feature

--- pkg/api/models/migrations.go
+++ pkg/api/models/migrations.go
@@ -4,7 +4,7 @@ package models
 func AutoMigrations() {
 	db := Connect()
 	defer db.Close()
-	db.DropTableIfExists(&Feedback{}, &User{})
+	//db.DropTableIfExists(&Feedback{}, &User{})
 	db.Debug().AutoMigrate(&User{}, &Feedback{})
 	db.Model(&Feedback{}).AddForeignKey("user_id", "users(id)", "cascade", "cascade")
 }
