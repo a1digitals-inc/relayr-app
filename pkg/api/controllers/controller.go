accd288a91f385574b8919b6ee40a10cd18717db
andre <andrleite@gmail.com>
2019-06-30 11:21:38 -0300

    adding prometheus metrics

--- pkg/api/controllers/controller.go
+++ pkg/api/controllers/controller.go
@@ -11,10 +11,3 @@ func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, `{"alive": true}`)
 }
-
-//MetricsHandler for prometheus metrics
-func MetricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, `{"data": 1}`)
-}
